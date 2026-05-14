ea893ec86660077bbb861a53afbc7527cb6f152c
andrew gamm <drewgamm@gmail.com>
2026-05-14 08:34:31 -0700

    fixed test

--- tests/test_user_age.py
+++ tests/test_user_age.py
@@ -8,6 +8,6 @@ from src.location import Location
 def test_year_first_in_dob_input():
     #ERROR FOUND HERE: WHEN MONTH == MONTH from current DATE, it AUTO SUBTRACTS 1
     test_user = UserProfile("Mike", "mike@gmail.com", "pass123", "05/04/2000", Location("Los Angeles", "CA", "U.S"))
-    assert test_user.get_age() == 25
+    assert test_user.get_age() == 26
     test_user_2 = UserProfile("Mike", "mike@gmail.com", "pass123", "05/01/2000", Location("Los Angeles", "CA", "U.S"))
     assert test_user_2.get_age() == 26
